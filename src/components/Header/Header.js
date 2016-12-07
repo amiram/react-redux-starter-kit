8f3b1c2da7d1370a6dfedb8663ddc8a4eddf99cc
amiram <amiramk@gmail.com>
2016-12-07 12:26:42 +0200

    article route

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,9 @@
-import React from 'react'
-import { IndexLink, Link } from 'react-router'
-import './Header.scss'
+import React from "react";
+import {IndexLink, Link} from "react-router";
+import "./Header.scss";
 
 export const Header = () => (
-  <div>
+  <div className="text-center">
     <h1>News reading app</h1>
     <IndexLink to='/' activeClassName='route--active'>
       Home
