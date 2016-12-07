cf57bae081a60c0a8b0195b7136e46879e5f1386
amiram <amiramk@gmail.com>
2016-12-07 12:17:24 +0200

    article route

--- src/routes/Article/components/Article.js
+++ src/routes/Article/components/Article.js
@@ -1,18 +1,32 @@
-import React from 'react'
-import { Link } from 'react-router'
+import React from "react";
+import {browserHistory} from "react-router";
 
-export const Article = (props) => (
-  <div style={{margin: '0 auto'}}>
-    <article>
-      <h1>{props.article.title}</h1>
-      <h5>{props.article.published.toString()}</h5>
-      <div>{props.article.text}</div>
-    </article>
-  </div>
-)
+class Article extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return this.props.article ?
+      <div style={{margin: '0 auto'}}>
+        <article>
+          <h1>{this.props.article.title}</h1>
+          <h5>{this.props.article.published.toString()}</h5>
+          <div>{this.props.article.text}</div>
+        </article>
+      </div>
+      : <br/>
+  }
+
+  componentWillMount() {
+    if (!this.props.article) {
+      browserHistory.push('/feed');
+    }
+  }
+}
 
 Article.propTypes = {
-  article: React.PropTypes.object.isRequired
+  article: React.PropTypes.object
 }
 
 export default Article
