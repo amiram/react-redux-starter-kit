5dc359422545fa800c7b967e6ea5dc71b3a12a0e
amiram <amiramk@gmail.com>
2016-12-07 10:28:25 +0200

    feed button

--- src/routes/Feed/components/Feed.js
+++ src/routes/Feed/components/Feed.js
@@ -3,7 +3,7 @@ import React from 'react'
 export const Feed = (props) => (
   <div style={{margin: '0 auto'}}>
     Feed
-    <button className="btn btn-default" onClick={this.dataRecieved.bind(this)}>Add Data</button>
+    <button className="btn btn-default" onClick={props.dataRecieved}>Add Data</button>
   </div>
 )
 
